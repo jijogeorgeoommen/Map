51cd95fe485680523c468944ec18386005b73180
jijogeorgeoommen <jijogeorgeoommen@gmail.com>
2019-10-24 08:55:34 +0530

    initia; commit

--- Map/ViewController.swift
+++ Map/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import MapKit
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mApviewoutlet: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        mApviewoutlet.mapType = MKMapType.satellite
+        
+        let location = CLLocationCoordinate2D(latitude: 8.5297, longitude: 76.9384)
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.05,longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mApviewoutlet.setRegion(region, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "hai"
+        annotation.subtitle = "TVM"
+        mApviewoutlet.addAnnotation(annotation)
+        
     }
 
 
